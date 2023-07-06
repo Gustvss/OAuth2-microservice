915a4850cc0c4224ab41b47aa84ae9dd6fb66cd9
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-07-06 13:42:04 -0300

    Configurate ms-oauth and add api-gateway project

--- api-gateway/src/main/java/com/tcser/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tcser/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.tcs.msoauth;
+package com.tcser.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MsOauthApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsOauthApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
---
+++ ms-oauth/src/main/java/com/tcser/msoauth/feignclients/UserFeignClient.java
@@ -0,0 +1,16 @@
+package com.tcser.msoauth.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.tcser.msoauth.entities.User;
+
+@Component
+@FeignClient(name = "ms-user", path = "/users")
+public interface UserFeignClient {
+    @GetMapping(value = "/search")
+    ResponseEntity<User> findByEmail(@RequestParam String email);
+}
+
